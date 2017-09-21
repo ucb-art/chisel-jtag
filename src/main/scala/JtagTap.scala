38b42ea0454ff9c073dddfccf4fa3d50f35bebcb
Stevo Bailey <stevo.bailey@eecs.berkeley.edu>
2017-09-21 14:12:51 -0700

    [stevo]: update syntax

--- src/main/scala/JtagTap.scala
+++ src/main/scala/JtagTap.scala
@@ -91,7 +91,7 @@ class JtagTapController(irLength: Int, initialInstruction: BigInt) extends Modul
   val nextActiveInstruction = Wire(UInt(irLength.W))
   val activeInstruction = NegativeEdgeLatch(clock, nextActiveInstruction, updateInstruction)   // 7.2.1d active instruction output latches on TCK falling edge
 
-  when (reset) {
+  when (reset.toBool) {
     nextActiveInstruction := initialInstruction.U(irLength.W)
     updateInstruction := true.B
   } .elsewhen (currState === JtagState.UpdateIR.U) {
